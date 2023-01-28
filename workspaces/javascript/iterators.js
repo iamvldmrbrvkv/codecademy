672448e23ca711cc3f96291bed4f5b6705103a2b
Владимир Боровиков <vladimirborovikov@192-168-0-184.local>
2023-01-28 14:47:16 +0300

    functions as parameters

--- workspaces/javascript/iterators.js
+++ workspaces/javascript/iterators.js
@@ -47,3 +47,59 @@ const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
   console.log(isTwoPlusTwo.name);
 
 // functions as parameters
+
+/* A higher-order function is a function that either accepts functions as parameters, returns a function, or both! 
+We call functions that get passed in as parameters callback functions. 
+Callback functions get invoked during the execution of the higher-order function.
+
+When we invoke a higher-order function, and pass another function in as an argument, we don’t invoke the argument function. 
+Invoking it would evaluate to passing in the return value of that function call. 
+With callback functions, we pass in the function itself by typing the function name without the parentheses:
+
+const higherOrderFunc = param => {
+  param();
+  return `I just invoked ${param.name} as a callback function!`
+}
+ 
+const anotherFunc = () => {
+  return 'I\'m being invoked by the higher-order function!';
+}
+ 
+higherOrderFunc(anotherFunc);
+
+We wrote a higher-order function higherOrderFunc that accepts a single parameter, param. 
+Inside the body, param gets invoked using parentheses. 
+And finally, a string is returned, telling us the name of the callback function that was passed in.
+
+Below the higher-order function, we have another function aptly named anotherFunc. 
+This function aspires to be called inside the higher-order function.
+
+Lastly, we invoke higherOrderFunc(), passing in anotherFunc as its argument, thus fulfilling its dreams of being called by the higher-order function.
+
+higherOrderFunc(() => {
+  for (let i = 0; i <= 10; i++){
+    console.log(i);
+  }
+});
+
+In this example, we invoked higherOrderFunc() with an anonymous function (a function without a name) that counts to 10. 
+Anonymous functions can be arguments too! */
+
+// example
+
+const addTwo = num => {
+  return num + 2;
+}
+
+const checkConsistentOutput = (func, val) => {
+const checkA = val + 2;
+const checkB = func(val);
+if (checkA === checkB) {
+  return func(val);
+} else {
+  return 'inconsistent results';
+}
+}
+
+
+console.log(checkConsistentOutput(addTwo, 10));
