40b69b174ba51ea9bfe07cbdc094eb2699004520
Владимир Боровиков <vladimirborovikov@192-168-0-184.local>
2023-02-02 17:58:37 +0300

    objects day 2.1

--- workspaces/javascript/objects.js
+++ workspaces/javascript/objects.js
@@ -155,3 +155,37 @@ Do object methods seem familiar? That’s because you’ve been using them all a
 
 We can include methods in our object literals by creating ordinary, colon-separated key-value pairs. The key serves as our method’s name, while the value is an anonymous function expression.
 */
+
+const alienShip = {
+    invade: function () { 
+      console.log('Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.')
+    }
+  };
+
+// With the new method syntax introduced in ES6 we can omit the colon and the function keyword.
+const alienShip = {
+    invade () { 
+      console.log('Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.')
+    }
+  };
+
+// Object methods are invoked by appending the object’s name with the dot operator followed by the method name and parentheses:
+
+alienShip.invade(); // Prints 'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+
+// example
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+const alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+}
+alienShip.retreat();
+alienShip.takeOff();
+
+// nested objects
