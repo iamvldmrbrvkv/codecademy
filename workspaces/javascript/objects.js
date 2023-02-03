9c646f8a86afe3e3f6488184721a899715119f3b
Владимир Боровиков <vladimirborovikov@192-168-0-184.local>
2023-02-03 14:25:23 +0300

    objects day 3

--- workspaces/javascript/objects.js
+++ workspaces/javascript/objects.js
@@ -189,3 +189,86 @@ alienShip.retreat();
 alienShip.takeOff();
 
 // nested objects
+
+/*
+In application code, objects are often nested— an object might have another object as a property which in turn could have a property that’s an array of even more objects!
+
+In our spaceship object, we want a crew object. 
+This will contain all the crew members who do important work on the craft. 
+Each of those crew members are objects themselves. 
+They have properties like name, and degree, and they each have unique methods based on their roles. 
+We can also nest other objects in the spaceship such as a telescope or nest details about the spaceship’s computers inside a parent nanoelectronics object.
+*/
+
+const spaceship = {
+  telescope: {
+     yearBuilt: 2018,
+     model: '91031-XLT',
+     focalLength: 2032 
+  },
+ crew: {
+     captain: { 
+         name: 'Sandra', 
+         degree: 'Computer Engineering', 
+         encourageTeam() { console.log('We got this!') } 
+      }
+ },
+ engine: {
+     model: 'Nimbus2000'
+  },
+  nanoelectronics: {
+      computer: {
+         terabytes: 100,
+         monitors: 'HD'
+      },
+     'back-up': {
+        battery: 'Lithium',
+        terabytes: 50
+      }
+ }
+}; 
+
+/*
+We can chain operators to access nested properties. 
+We’ll have to pay attention to which operator makes sense to use in each layer. 
+It can be helpful to pretend you are the computer and evaluate each expression from left to right so that each operation starts to feel a little more manageable.
+*/
+
+spaceship.nanoelectronics['back-up'].battery; // Returns 'Lithium'
+
+// In the preceding code:
+// First the computer evaluates spaceship.nanoelectronics, which results in an object containing the back-up and computer objects.
+// We accessed the back-up object by appending ['back-up'].
+// The back-up object has a battery property, accessed with .battery which returned the value stored there: 'Lithium'
+
+// example
+let spaceship = {
+  passengers: [{name: 'Vladimir'}],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+const capFave = spaceship.crew.captain['favorite foods'][0];
+const firstPassenger = spaceship.passengers[0];
