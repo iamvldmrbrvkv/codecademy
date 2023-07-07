133bc5ece3668c651c262bc14addd50e3af9601d
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-07-07 11:00:25 +0300

    fixed withAsync app

--- paths/full-stack-engineer/front-end-development/async-javascript-and-http-requests/async-await/withAsync-app/app.js
+++ paths/full-stack-engineer/front-end-development/async-javascript-and-http-requests/async-await/withAsync-app/app.js
@@ -1,39 +1,38 @@
 function withConstructor(num){
-    return new Promise((resolve, reject) => {
-      if (num === 0){
-        resolve('zero');
-      } else {
-        resolve('not zero');
-      }
-    });
-  }
-  
-  withConstructor(0)
-    .then((resolveValue) => {
-    console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
-  });
-  
-  // Write your code below:
-  async function withAsync (num) {
-    if (num === 0) {
-      return 'zero';
+  return new Promise((resolve, reject) => {
+    if (num === 0){
+      resolve('zero');
     } else {
-      return 'not zero';
+      resolve('not zero');
     }
+  });
+}
+
+withConstructor(0)
+  .then((resolveValue) => {
+  console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+});
+
+// Write your code below:
+async function withAsync(num) {
+  if (num === 0) {
+    return 'zero';
+  } else {
+    return 'not zero';
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // Leave this commented out until step 3:
-  
-  withAsync(100)
-    .then((resolveValue) => {
-    console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
-  })
-  
+}
+
+
+
+
+
+
+
+
+
+// Leave this commented out until step 3:
+
+withAsync(100)
+  .then((resolveValue) => {
+  console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+})
